18f6aa551b781db2bac04f1b42c4595c3218f713
Daanjels <admin@daanjels.be>
2020-07-11 23:28:04 +0200

    config restored

---
+++ src/config/config.php
@@ -0,0 +1,13 @@
+<?php
+// define url root as a constant // without the slash
+define('URLROOT', 'http://localhost/daanjels-mvc'); // adapt as needed
+// site name
+define('SITENAME', 'da:njels'); // adapt as needed
+// DB params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'sqluser'); // adapt as needed
+define('DB_PASS', 'password'); // adapt as needed
+define('DB_NAME', 'database'); // adapt as needed
+
+// define app root as a constant
+define('APPROOT', dirname(dirname(__FILE__)));
