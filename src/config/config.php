a924d19e5fc125c751fc53eeae391cda36f61a39
Daanjels <admin@daanjels.be>
2020-07-11 23:10:01 +0200

    Config adapted

--- src/config/config.php
+++ src/config/config.php
@@ -5,9 +5,9 @@ define('URLROOT', 'http://localhost/daanjels-mvc'); // adapt as needed
 define('SITENAME', 'da:njels'); // adapt as needed
 // DB params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'phpuser'); // adapt as needed
-define('DB_PASS', 'AzErTy&2"4('); // adapt as needed
-define('DB_NAME', 'mydatabase'); // adapt as needed
+define('DB_USER', 'sqluser'); // adapt as needed
+define('DB_PASS', 'password'); // adapt as needed
+define('DB_NAME', 'database'); // adapt as needed
 
 // define app root as a constant
 define('APPROOT', dirname(dirname(__FILE__)));
