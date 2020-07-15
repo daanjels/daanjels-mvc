abbdcb6d485c2e5105a1189483e0e55eb6cb1b4e
Daanjels <admin@daanjels.be>
2020-07-15 09:53:37 +0200

    created a config-dev for the github repository

---
+++ src/config/config-dev.php
@@ -0,0 +1,16 @@
+<?php
+// remove '-dev' from the name of this file to start using it in this project
+
+// define url root as a constant // without the slash
+define('URLROOT', 'http://localhost/daanjels-mvc'); // adapt as needed
+// site name
+define('SITENAME', 'da:njəls'); // adapt as needed
+// DB params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'sqluser'); // adapt as needed
+define('DB_PASS', 'password'); // adapt as needed
+define('DB_NAME', 'database'); // adapt as needed
+
+// define app root as a constant
+define('APPROOT', dirname(dirname(__FILE__)));
+define('MEDIAROOT', URLROOT.'/public/media');
