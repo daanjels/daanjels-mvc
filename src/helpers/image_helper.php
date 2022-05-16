f02b9dec8f5332479c72ac138db86b289995e597
Wim Daniëls <daanjels@iMac-van-Wim.local>
2022-05-16 15:56:47 +0200

    updatestyle2022

--- src/helpers/image_helper.php
+++ src/helpers/image_helper.php
@@ -1,28 +1,28 @@
 <?php
 
-function insertsmallPicture($selectedImage, $alt)
+function insertsmallPicture($selectedImage, $alt, $extra="")
 {
     $insert = '<picture>' .
         '<source media="(max-width: 512px)" srcset="'.$selectedImage.'_128_.jpg">' .
         '<source media="(max-width: 1024px)" srcset="'.$selectedImage.'_256_.jpg">' .
         '<source media="(max-width: 1408px)" srcset="'.$selectedImage.'_384_.jpg">' .
-        '<img src="'.$selectedImage.'_512_.jpg" alt="'.$alt.'"/>' .
+        '<img src="'.$selectedImage.'_512_.jpg" alt="'.$alt.'" '.$extra.'/>' .
         '</picture>';
 				return $insert;
 }
 
-function inserthalfPicture($selectedImage, $alt)
+function inserthalfPicture($selectedImage, $alt, $extra="")
 {
 	$insert = '<picture>' .
 	'<source media="(max-width: 512px)" srcset="'.$selectedImage.'_256_.jpg">' .
 	'<source media="(max-width: 1024px)" srcset="'.$selectedImage.'_512_.jpg">' .
 	'<source media="(max-width: 1408px)" srcset="'.$selectedImage.'_768_.jpg">' .
-	'<img src="'.$selectedImage.'_1024_.jpg" alt="'.$alt.'"/>' .
+	'<img src="'.$selectedImage.'_1024_.jpg" alt="'.$alt.'" '.$extra.'/>' .
 	'</picture>';
 	return $insert;
 }
 
-function insertPicture($selectedImage, $alt)
+function insertPicture($selectedImage, $alt, $extra="")
 {
     $insert = '<picture>' .
         '<source media="(max-width: 384px)" srcset="'.$selectedImage.'_384_.jpg">' .
@@ -30,7 +30,7 @@ function insertPicture($selectedImage, $alt)
         '<source media="(max-width: 768px)" srcset="'.$selectedImage.'_768_.jpg">' .
         '<source media="(max-width: 1024px)" srcset="'.$selectedImage.'_1024_.jpg">' .
         '<source media="(max-width: 1408px)" srcset="'.$selectedImage.'_1408_.jpg">' .
-        '<img src="'.$selectedImage.'.jpg" alt="'.$alt.'"/>' .
+        '<img src="'.$selectedImage.'.jpg" alt="'.$alt.'" '.$extra.'/>' .
         '</picture>';
     return $insert;
 }
